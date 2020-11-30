22c15a877bc83d1072582f9e0749e2a7e7e0d514
yrenyi0603@gmail.com <yrenyi0603@gmail.com>
2020-11-30 14:01:29 +0800

    配置部署方式

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -37,10 +37,10 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # try:
+    #     # os.makedirs(app.instance_path)
+    # except OSError:
+    #     pass
 
     # a simple page that says hello
     @app.route('/hello')
